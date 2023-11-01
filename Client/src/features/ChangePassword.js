7454c6ef60955720bad8408329618837b9554571
Hung186 <133014679+Hanalabama@users.noreply.github.com>
2023-11-01 15:49:55 +0700

    Revert "Revert "Call API ChangePassword, UpdateProfile""
    This reverts commit e94d78a1f8ab63664322f6b9e7bffe3bb1edd924.

--- Client/src/features/ChangePassword.js
+++ Client/src/features/ChangePassword.js
@@ -1,48 +1,84 @@
-import { View, Text, TouchableOpacity,Image, StyleSheet, Dimensions, TextInput} from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity,Image, StyleSheet, Dimensions, TextInput, ToastAndroid} from 'react-native'
+import React ,{ useState, useContext } from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
+import AxiosIntance from './AxiosIntance';
+import { AppConText } from './AppConText';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ChangePassword = () => {
-  return (
-    // Error: khi nhấp vào textinput sẽ bị đẩy lên trên
+const ChangePassword = (props) => {
+
+    const { navigation } = props;
+    const { setisLogin } = useContext(AppConText);
+    const [password, setPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    
+
+    const ChangePassword = async () => {
+        try {
+            await AxiosIntance().patch("users/change-password", { 
+                password: password,
+                newpassword: newPassword,
+                passwordconfirm: passwordConfirm
+            });
+            ToastAndroid.show("Change Password Success", ToastAndroid.SHORT);
+            //quay lại màn hình login
+            setisLogin(false);
+            await AsyncStorage.removeItem("token");
+        } catch (e) {
+            ToastAndroid.show(e.response.data.message, ToastAndroid.SHORT);
+        }
+
+    }
+
+    const goBack = () =>{
+        navigation.goBack();
+    }
+
+    return (
+    
     <View style={styles.container}>
         {/* Phần đầu */}
         <View style={styles.header}>   
-            <TouchableOpacity>
+            <TouchableOpacity onPress={goBack}>
                 <Image style={styles.imagestyle} source={require('../icons/back.png')} />
-                </TouchableOpacity>               
+            </TouchableOpacity>               
             <View style={{flex:1, justifyContent:'center', marginLeft:-50}}>
                 <Text style={[styles.textstyle, {textAlign:'center'}]}>Change Password</Text>
             </View>
         </View>
         {/* Phần nhập thông tin người dùng*/}
         <View style={styles.infocontainer}>
-            {/* Phần nhập email */}
-            <Text style={styles.textstyle}>Email</Text>
+            {/* Phần nhập mật khẩu cũ */}
+            <Text style={styles.textstyle}>Current password:</Text>
             <TextInput
                 style={styles.textinput}
-                placeholder="Enter email"
+                placeholder="Enter current password"
                 placeholderTextColor={'white'}
-                keyboardType='email-address'
                 returnKeyType='next'
+                secureTextEntry={true}
                 autoCorrect={false} 
+                onChangeText={setPassword}
             />
-            {/* Phần nhập mật khẩu */}
-            <Text style={styles.textstyle}>Password</Text>
+            {/* Phần nhập mật khẩu mới */}
+            <Text style={styles.textstyle}>Password:</Text>
             <TextInput 
                 style={styles.textinput}
-                placeholder="Enter password"
-                placeholderTextColor={'white'}
-                secureTextEntry={true}
-                autoCorrect={false} />
-            {/* Phần nhập lại mật khẩu */}
-            <Text style={styles.textstyle}>Confirm Password</Text>
-            <TextInput 
-                style={styles.textinput}
-                placeholder="Confirm password"
+                placeholder="Enter new password"
                 placeholderTextColor={'white'}
                 secureTextEntry={true}
                 autoCorrect={false} 
+                onChangeText={setNewPassword}
+                />
+            {/* Phần nhập lại mật khẩu mới */}
+            <Text style={styles.textstyle}>Confirm Password:</Text>
+            <TextInput 
+                style={styles.textinput}
+                placeholder="Enter Confirm password"
+                placeholderTextColor={'white'}
+                secureTextEntry={true}
+                autoCorrect={false} 
+                onChangeText={setPasswordConfirm}
                 />
             {/* Nút xác nhận đổi mật khẩu */}
             <LinearGradient
@@ -51,7 +87,7 @@ const ChangePassword = () => {
                 end={{ x: 1.0, y: 0.0 }}
                 locations={[0.0, 2.0]}
                 colors={['#F34C30', '#DA004E']}>
-                <TouchableOpacity style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                <TouchableOpacity style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}} onPress={ChangePassword}>
                     <Image style={{marginRight:'5%'}} source={require('../icons/password.png')}></Image>
                     <Text style={styles.textstyle2}>Change Password</Text>
                 </TouchableOpacity>
@@ -65,7 +101,8 @@ const ChangePassword = () => {
 export default ChangePassword
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
         flexDirection:'column',
         backgroundColor:'black',
         padding:'10%'
